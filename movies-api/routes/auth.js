7de6a44d54a93acc4dbd8e023640997357dd9c65
Guillermo Rodas <garethderioth@gmail.com>
2019-07-16 18:37:58 -0500

    Implementacion de nuestro sign up

--- movies-api/routes/auth.js
+++ movies-api/routes/auth.js
@@ -3,6 +3,10 @@ const passport = require('passport');
 const boom = require('@hapi/boom');
 const jwt = require('jsonwebtoken');
 const ApiKeysService = require('../services/apiKeys');
+const UsersService = require('../services/users');
+const validationHandler = require('../utils/middleware/validationHandler');
+
+const { createUserSchema } = require('../utils/schemas/users');
 
 const { config } = require('../config');
 
@@ -14,6 +18,7 @@ function authApi(app) {
   app.use('/api/auth', router);
 
   const apiKeysService = new ApiKeysService();
+  const usersService = new UsersService();
 
   router.post('/sign-in', async function(req, res, next) {
     const { apiKeyToken } = req.body;
@@ -58,6 +63,25 @@ function authApi(app) {
         next(error);
       }
     })(req, res, next);
+  });
+
+  router.post('/sign-up', validationHandler(createUserSchema), async function(
+    req,
+    res,
+    next
+  ) {
+    const { body: user } = req;
+
+    try {
+      const createdUserId = await usersService.createUser({ user });
+
+      res.status(201).json({
+        data: createdUserId,
+        message: 'user created'
+      });
+    } catch (error) {
+      next(error);
+    }
   });
 }
 
