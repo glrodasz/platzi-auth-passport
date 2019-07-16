cfd8c55902606013143828edd7463d73f3f8ecbf
Guillermo Rodas <garethderioth@gmail.com>
2019-07-16 17:36:13 -0500

    Implementacion de estrategia JWT en passport.js

---
+++ movies-api/utils/auth/strategies/jwt.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const boom = require('@hapi/boom');
+
+const UsersService = require('../../../services/users');
+const { config } = require('../../../config');
+
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: config.authJwtSecret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    },
+    async function(tokenPayload, cb) {
+      const usersService = new UsersService();
+
+      try {
+        const user = await usersService.getUser({ email: tokenPayload.email });
+
+        if (!user) {
+          return cb(boom.unauthorized(), false);
+        }
+
+        delete user.password;
+
+        cb(null, { ...user, scopes: tokenPayload.scopes });
+      } catch (error) {
+        return cb(error);
+      }
+    }
+  )
+);
