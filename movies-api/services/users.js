9cd7ae9a862eae7e07a55164763d006d5580b918
Guillermo Rodas <garethderioth@gmail.com>
2019-07-16 15:34:04 -0500

    Agregando la coleccion de usuario

---
+++ movies-api/services/users.js
@@ -0,0 +1,29 @@
+const MongoLib = require('../lib/mongo');
+const bcrypt = require('bcrypt');
+
+class UsersService {
+  constructor() {
+    this.collection = 'users';
+    this.mongoDB = new MongoLib();
+  }
+
+  async getUser({ email }) {
+    const [user] = await this.mongoDB.getAll(this.collection, { email });
+    return user;
+  }
+
+  async createUser({ user }) {
+    const { name, email, password } = user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createUserId = await this.mongoDB.create(this.collection, {
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    return createUserId;
+  }
+}
+
+module.exports = UsersService;
