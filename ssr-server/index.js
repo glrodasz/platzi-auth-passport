dea8f8283d3540fdc38f460d8e0ccc18a77e6e05
Guillermo Rodas <garethderioth@gmail.com>
2019-07-17 16:18:50 -0500

    Implementacion de las rutas de las peliculas

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -27,6 +27,8 @@ app.post("/auth/sign-in", async function(req, res, next) {
           next(error);
         }
 
+        const { token, ...user } = data;
+
         res.cookie("token", token, {
           httpOnly: !config.dev,
           secure: !config.dev
@@ -58,9 +60,48 @@ app.post("/auth/sign-up", async function(req, res, next) {
 
 app.get("/movies", async function(req, res, next) {});
 
-app.post("/user-movies", async function(req, res, next) {});
+app.post("/user-movies", async function(req, res, next) {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
 
-app.delete("/user-movies/:userMovieId", async function(req, res, next) {});
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "post",
+      data: userMovie
+    });
+
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/user-movies/:userMovieId", async function(req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "delete"
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
